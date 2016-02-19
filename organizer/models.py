cda5470dbed0e6a2d9a5e9346b34c530771238eb
Jian.Ma <jian.ma@dianrong.com>
2016-02-19 17:16:53 +0800

    Add required fields to models.

--- organizer/models.py
+++ organizer/models.py
@@ -2,23 +2,22 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=31)
-    slug = models.SlugField()
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config.')
 
 
 class Startup(models.Model):
     name = models.CharField(max_length=31)
     slug = models.SlugField()
     description = models.TextField()
-    founded_date =models.DateField()
+    founded_date = models.DateField()
     contact = models.EmailField()
     website = models.URLField()
+    tags = models.ManyToManyField(Tag)
 
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=63)
-    pub_date = models.DateField()
-    link = models.URLField
-
-
-
+    pub_date = models.DateField('date published')
+    link = models.URLField()
+    startup = models.ForeignKey(Startup)
